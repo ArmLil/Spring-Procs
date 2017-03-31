5e9bba9847b5baae9afb6dc871ab9402d283256d
ArmLil <armlilhov@mail.ru>
2017-03-31 16:01:52 +0400

    shorten some variables' names

--- index.js
+++ index.js
@@ -9,67 +9,55 @@ const spring = express();
 spring.post('/check-procs', (req, res) => {
 })
 
-const get_row_obj =  (index_row, array) => {
-  let string = '';
-  //we need to have the first word of
-  // each row for further filtering
-  let first_word = '';
-  let numb = index_row;
-
+const get_row_obj =  (i_row, arr) => {
+  //we need to have the first word of each row for further filtering
+  let str = '', f_word = '', n = i_row;
   do{
-    first_word += array[numb++];
-  }while(array[numb] !== ' ')
-
+    f_word += arr[n++];
+  }while(arr[n] !== ' ')
   do{
-    string += array[index_row++];//get the row
-  }while(array[index_row] !== '\n')
-  // index_row shows the last index of each row
-  let result = [++index_row, first_word, string];
+    str += arr[i_row++];//get the row
+  }while(arr[i_row] !== '\n')
+  let result = [++i_row, f_word, str];// i_row is the last index of each row
   return result;
 };
 
-
 const command_who = () => {
   const user = child.spawn('whoami');
-  let array = [];
-  user.stdout.on('data', (data) => {
-    array.push(data);
-  });
-
+  let arr = [];
+  user.stdout.on('data', (d) => arr.push(d));
   user.stdout.on('end', () => {
-    array = Buffer.concat(array).toString();
-    let numb = 0;
-    let user_name = '';
+    arr = Buffer.concat(arr).toString();
+    let n = 0, user_name = '';
     do{
-      user_name += array[numb++];
-    }while(array[numb] !== '\n')
+      user_name += arr[n++];
+    }while(arr[n] !== '\n')
+    console.log(user_name);
   });
  }
+command_who();
 
- const command_ps = () => {
+const command_ps = () => {
    const ps = child.spawn('ps',['aux']);
-   let array = [];
-   ps.stdout.on('data', (data) => {
-       array.push(data);
-   });
+   let arr = [];
+   ps.stdout.on('data', (d) => arr.push(d));
    ps.stdout.on('end', () => {
-     array = Buffer.concat(array).toString();
-     let array_of_rows = [];
-     let index_row = 0;
+     arr = Buffer.concat(arr).toString();
+     let arr_row = [], i_row = 0;
      let str_obj = [];
      do{
-       str_obj = get_row_obj(index_row, array);
+       str_obj = get_row_obj(i_row, arr);
        //here need to compare with the result of whoami not with 'lilo'
        //str_obj[1] is the first word of the row,
        // str_obj[0] is first index of each row
-       if(str_obj[1] !== 'lilo' && str_obj[1] !== 'root'){
-          array_of_rows.push(str_obj[2]);
-       }
-       index_row = str_obj[0];
-     }while(index_row < array.length);
+       if(str_obj[1] !== 'lilo' && str_obj[1] !== 'root')
+          arr_row.push(str_obj[2]);
+       i_row = str_obj[0];
+     }while(i_row < arr.length);
 
-     //for(const a of array_of_rows){
-       //console.log(`${a}\n`);
-     //}
+     for(const a of arr_row){
+       console.log(`${a}\n`);
+     }
    });
  }
+ command_ps();
