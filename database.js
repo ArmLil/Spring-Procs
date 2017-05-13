fb7ae66a6d7d02e8fce37603deb0c740f31571c1
ArmLil <armlilhov@mail.ru>
2017-05-13 12:35:40 +0400

    add db.sql file (first commit for that)

--- database.js
+++ database.js
@@ -1,28 +1,18 @@
 'use  strict'
 
-const dbFile = './database.db';
-const fs          = require('fs');
-
-const dbExists = fs.existsSync(dbFile);
-if(!dbExists)  fs.openSync(dbFile, 'w');
-
 const sqlite3     = require('sqlite3').verbose();
-const db = new sqlite3.Database(dbFile);
+const db = new sqlite3.Database('database.db');
 
-
-exports.get_db = async (last_arr) => {
+exports.get_db = (last_arr) => {
   return new Promise((resolve, reject) => {
 
     db.serialize(() => {
-      db.run('CREATE TABLE if not exists my_table (' +
-      '`run_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' +
-      '`result` TEXT NOT NULL)');
       // Insert some data using a statement:
       let obj_arr = [];
       const statement = db.prepare('INSERT INTO `my_table` (`result`) VALUES (?)');
          for (let row = 0; row < last_arr.length; ++row){
            obj_arr.push(`
-{ROW: ${row +1}, USER: ${last_arr[row][0]}, PID: ${last_arr[row][1]}, COMMAND: ${last_arr[row][2]}}`);
+{ROW: ${row + 1}, USER: ${last_arr[row][0]}, PID: ${last_arr[row][1]}, COMMAND: ${last_arr[row][2]}}`);
            }
       statement.run(`${obj_arr}`);
       statement.finalize();
@@ -37,6 +27,5 @@ exports.get_db = async (last_arr) => {
       // Close the database:
       db.close();
     });
-
   });
 };
---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE if not exists my_table (
+    run_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+
+    result TEXT NOT NULL
+);
